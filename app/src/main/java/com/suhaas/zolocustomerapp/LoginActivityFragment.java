845eec2b5b88973aa34048f837bc5dd8196f2636
Suhaas S <suhaas.sreedhar@gmail.com>
2017-07-20 16:45:09 +0530

    Initial commit

---
+++ app/src/main/java/com/suhaas/zolocustomerapp/LoginActivityFragment.java
@@ -0,0 +1,23 @@
+package com.suhaas.zolocustomerapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LoginActivityFragment extends Fragment {
+
+    public LoginActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
